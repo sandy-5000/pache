ce681808e6c89f2e1bfe5e6b0d0cfe0e6f96a5e6
Sandeep Kumar Bhaviri <sandeepkumarbhaviri@gmail.com>
2026-04-18 21:01:30 +0530

    added cache dir

--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
 #include "constants/page.h"
+#include <errno.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
+    const char *dir = "p_cache";
+
+    if (mkdir(dir, 0755) == -1) {
+        if (errno != EEXIST) {
+            perror("mkdir failed");
+            return 1;
+        }
+    }
+
     printf("Persistant Cache - **pache!**\n");
     printf("Page Size - %d\n", PAGE_SIZE);
+    printf("Directory '%s' ready\n", dir);
+
     return 0;
 }
